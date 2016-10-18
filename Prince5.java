719eb162f6f3140de5f58cbf45db5b430c9216dd
princeela <princeela1710@gmail.com>
2016-10-18 10:27:20 +0530

    Add files via upload

---
+++ Prince5.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prince5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author KSRIET
+ */
+public class Prince5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int a,b;
+		System.out.println("Enter the first number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+		
+System.out.println(a!=b);  // TODO code application logic here
+    }
+    
+}
